f02dd2bfc44496ed3a5938faa4c554c4dbb04c9a
Nikola Hristov <nikola@nikolahristov.tech>
2023-05-21 17:51:49 +0300

    squash!

---
+++ dist/lib/flights.d.ts
@@ -0,0 +1,5 @@
+import type { optionCommandsPlan } from "../options/index.js";
+export default class flights {
+    plan: optionCommandsPlan;
+    constructor(debug?: optionCommandsPlan["debug"]);
+}
