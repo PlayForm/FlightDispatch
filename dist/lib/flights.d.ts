07817e91ebf748a532b4f7ed27b162951761df0c
Nikola Hristov <nikola@nikolahristov.tech>
2023-07-30 20:14:43 +0300

    squash!

--- dist/lib/flights.d.ts
+++
@@ -1,5 +0,0 @@
-import type { optionCommandsPlan } from "../options/index.js";
-export default class flights {
-    plan: optionCommandsPlan;
-    constructor(debug?: optionCommandsPlan["debug"]);
-}
--- dist/lib/flights.js
+++
@@ -1,1 +0,0 @@
-class o{plan={flights:0,debug:2,info:{},results:new Map,ongoing:{destination:""}};constructor(n=2){this.plan.debug=n}}export{o as default};
