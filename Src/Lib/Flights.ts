9ddbf3c8950b944f786c6d73c701c9ff2219b571
Nikola Hristov <nikola@nikolahristov.tech>
2023-08-04 04:29:47 +0300

    squash!

--- Src/Lib/Flights.ts
+++ Src/Lib/Flights.ts
@@ -3,7 +3,6 @@ import type { optionCommandsPlan } from "../options/Index.js";
 export default class flights {
 	plan: optionCommandsPlan = {
 		flights: 0,
-
 		debug: 2,
 		info: {},
 		results: new Map(),
