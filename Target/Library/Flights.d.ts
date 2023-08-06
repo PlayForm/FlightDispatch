87dbcd4253e0fd9c5c7e35d98f9a92733ab33df7
Nikola Hristov <nikola@nikolahristov.tech>
2023-08-06 20:18:45 +0300

    squash!

---
+++ Target/Library/Flights.d.ts
@@ -0,0 +1,5 @@
+import type { optionCommandsPlan } from "../Option/Index.js";
+export default class flights {
+    plan: optionCommandsPlan;
+    constructor(debug?: optionCommandsPlan["debug"]);
+}
---
+++ Target/Option/Index.d.ts
@@ -0,0 +1,37 @@
+/// <reference types="node" />
+export type optionDebug = 0 | 1 | 2;
+export type optionInfo = string;
+export interface commands {
+    Fulfilled?: boolean | ((plan: optionCommandsPlan) => Promise<false | string>);
+    Failed?: boolean | ((inputPath: optionCommandsFlight) => Promise<false | string>);
+    Accomplished?: boolean | ((ongoing: optionCommandsFlight) => Promise<false | string>);
+    changed?: (plan: optionCommandsPlan) => Promise<optionCommandsPlan>;
+    passed?: (ongoing: optionCommandsFlight) => Promise<Boolean>;
+}
+export type optionExclude = string | RegExp | ((file: string) => boolean);
+export type optionPath = string | URL | Map<string | URL, string | URL> | false;
+export interface Options {
+    [key: string]: any;
+    path?: optionPath | optionPath[] | Set<optionPath>;
+    exclude?: optionExclude | optionExclude[] | Set<optionExclude>;
+    dispatch?: commands;
+    logger?: optionDebug;
+}
+export interface optionCommandsPlan {
+    debug: optionDebug;
+    flights: number;
+    info: any;
+    results: Map<string, string>;
+    ongoing: optionCommandsFlight;
+}
+export interface optionCommandsFlight {
+    destination: optionInfo;
+}
+declare const _default: {
+    logger: 2;
+    dispatch: {
+        passed: () => Promise<boolean>;
+        changed: (plan: optionCommandsPlan) => Promise<optionCommandsPlan>;
+    };
+};
+export default _default;
